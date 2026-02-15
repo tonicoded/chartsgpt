fccf033bf9ee423865378de682fb00398edbd40d
Anthony Verruijt <anthonyverruijt@mac.home>
2026-02-15 11:36:38 +0100

    fd

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,7 @@ const nextConfig = {
   trailingSlash: true,
   experimental: {
     optimizePackageImports: ["@vercel/analytics"]
-  },
-  outputFileTracingIncludes: {
-    "/*": ["./index.html", "./about/**/*", "./privacy/**/*", "./terms/**/*", "./support/**/*", "./blog/**/*", "./styles.css"]
   }
 };
 
 export default nextConfig;
-
